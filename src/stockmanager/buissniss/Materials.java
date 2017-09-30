17b4473558ea2e4acc7d942133d1c9fe995509c5
Kenny <kenny.devex@gmail.com>
2017-09-30 15:55:34 -0100

    Este é a classe de negocio de sistema

---
+++ src/stockmanager/buissniss/Materials.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stockmanager.buissniss;
+
+import java.util.Date;
+
+/**
+ *
+ * @author kenny
+ */
+public class Materials {
+    int num;
+    String name;
+    float price;
+    Date date;
+    
+}
